74caf962023c4ad4b94d556873256f51866b3a28
FreekManders <freek.manders@gmail.com>
2021-12-24 10:51:16 +0100

    VDJ regions are now highlighted in the regional cosine similarity plot.

--- scripts/Run_ballstjude.R
+++ scripts/Run_ballstjude.R
@@ -49,6 +49,7 @@ saveRDS(sims_ext, "sims_ext.rds")
 sims_noext = determine_regional_similarity(gr, ref_genome, chromosomes, window_size = 200, stepsize = 50, oligo_correction = FALSE, extension = 0)
 saveRDS(sims_noext, "sims_noext.rds")
 
+
 # Plot results
 sims_fig = plot_regional_similarity(sims)
 ggsave("sims.pdf", sims_fig)
@@ -59,8 +60,39 @@ ggsave("sims_ext.pdf", sims_fig_ext)
 sims_fig_noext = plot_regional_similarity(sims_noext)
 ggsave("sims_no_ext.pdf", sims_fig_noext)
 
+# Plot results with VDJ annotation
+# Location IGH, IGK and IGL
+# From Entrez genes hg19.
+# https://www.ncbi.nlm.nih.gov/gene/3492#genomic-context
+# https://www.ncbi.nlm.nih.gov/gene/50802#genomic-context
+# https://www.ncbi.nlm.nih.gov/gene/3535#genomic-context
+vdj_sites = c(mean(c(89.156874, 90.274235)),
+              mean(c(106.052774, 107.288051)),
+              mean(c(22.380474, 23.265085))
+)
+sims_fig_annotated = sims_fig + 
+    geom_vline(data = data.frame(chr = factor(c("2", "14", "22"), levels = c(1:22, "X"))), 
+               aes(xintercept = vdj_sites), alpha = 0.5, colour = "red", size = 1)
+ggsave("sims_annotated.pdf", sims_fig_annotated)
 
+### Calculate cosine similarity of VDJ regions with signatures
 
+# Get mutations in vdj region
+vdj_gr = GRanges(seqnames = c("chr2", "chr14"), 
+                 ranges = IRanges(start = c(89156874, 106052774), 
+                                  end = c(90274235, 107288051)))
+overlaps = findOverlaps(gr, vdj_gr)
+vdj_muts_gr = gr[queryHits(overlaps)]
 
+# Determine similarity
+mut_mat_vdj = mut_matrix(vdj_muts_gr, ref_genome)
+signatures = get_known_signatures()
+cos_sim = cos_sim_matrix(mut_mat_vdj, signatures)
+res = fit_to_signatures_strict(mut_mat_vdj, signatures)
+plot_contribution(res$fit_res$contribution)
 
-
+sims_cossims = sims@sim_tb %>% dplyr::arrange(cossim)
+sims_cossims = sims_cossims[c(1,3),]
+vdj_gr = GRanges(seqnames = as.vector(sims_cossims$chr), 
+                 ranges = IRanges(start = sims_cossims$window_begin, 
+                                  end = sims_cossims$window_end))
